c399a1b4d8c4f4a04b0301ab827ad38a2fdbff7e
Yanikovic <schroeder_yannik@web.de>
2025-02-11 08:28:15 +0100

    Start measure when all clients are connected

--- benchmark.cpp
+++ benchmark.cpp
@@ -3,50 +3,72 @@
 #include <syncstream>
 #include <vector>
 
-#include <boost/system/error_code.hpp>
 #include <asio.hpp>
 #include <numeric>
 #include <thread>
 
 using asio::ip::tcp;
-constexpr std::string host = "127.0.0.1";
-constexpr uint16_t port = 12345;
+using namespace std::chrono_literals;
+
+constexpr std::string SERVER_HOST = "127.0.0.1";
+constexpr uint16_t SERVER_PORT = 12345;
 constexpr size_t FOUR_KiB = 4 * 1024;
 
-auto info() { return std::osyncstream(std::cout); }
+std::atomic<int> num_connected = 0;
+std::atomic_bool shutdown_signal = false;
 
+auto info() { return std::osyncstream(std::cout); }
 auto debug() { return std::osyncstream(std::cerr); }
 
 asio::awaitable<void> send(tcp::socket socket) {
-    std::vector buf(FOUR_KiB, 'A');
+    // info() << "Client IP/PORT: " << socket.remote_endpoint().address() << ":" << socket.remote_endpoint().port()
+    //         << std::endl;
+    const std::vector buf(FOUR_KiB, 'A');
+
     while (true) {
         [[maybe_unused]] auto [ec, n] =
                 co_await async_write(socket, asio::const_buffer(buf.data(), buf.size()), asio::as_tuple);
 
         if (ec) {
             socket.shutdown(tcp::socket::shutdown_both, ec);
-            info() << "shutdown: " << ec.message() << std::endl;
+            // info() << "shutdown: " << ec.message() << std::endl;
             break;
         }
         // info() << "send: " << ec.message() << std::endl;
     }
 }
 
-asio::awaitable<void> async_client(const bool &stopped, uint64_t &bytesRead) try {
-    tcp::socket socket{co_await asio::this_coro::executor};
+asio::awaitable<void> async_client(const tcp::endpoint client_endpoint, const int num_connections, uint64_t &bytesRead) try {
+    const auto executor = co_await asio::this_coro::executor;
+    asio::steady_timer timer{executor};
+    tcp::socket socket{executor};
+    socket.open(tcp::v4());
+    socket.bind(client_endpoint);
 
-    co_await socket.async_connect({{}, port});
+    co_await socket.async_connect({asio::ip::address_v4::from_string(SERVER_HOST), SERVER_PORT});
+    ++num_connected;
+    while (num_connected.load() < num_connections) {
+        timer.expires_from_now(10ms);
+        co_await timer.async_wait(asio::use_awaitable);
+    }
 
     std::vector<char> buf(FOUR_KiB);
-    while (!stopped) {
+    while (!shutdown_signal.load()) {
         auto [ec, n] = co_await async_read(
             socket,
             asio::mutable_buffer(buf.data(), buf.size()),
             asio::as_tuple);
         bytesRead += n;
     }
+
+    asio::error_code ec;
+    socket.shutdown(tcp::socket::shutdown_both, ec);
+    if (ec) {
+        debug() << "shutdown: " << ec.message() << std::endl;
+    }
+    socket.close();
 } catch (asio::system_error const &se) {
-    debug() << "read:" << se.code().message() << std::endl;
+    debug() << "async_client: " << se.code().message() << std::endl;
 }
 
 void blocking_client(const bool &stopped, uint64_t &bytesRead) {
@@ -61,8 +83,8 @@ void blocking_client(const bool &stopped, uint64_t &bytesRead) {
     hints.ai_protocol = 0;
     /// specifying 0 in this field indicates that socket addresses with any protocol can be returned by getaddrinfo();
 
-    if (const auto errorCode = getaddrinfo(host.c_str(),
-                                           std::to_string(port).c_str(),
+    if (const auto errorCode = getaddrinfo(SERVER_HOST.c_str(),
+                                           std::to_string(SERVER_PORT).c_str(),
                                            &hints,
                                            &result); errorCode != 0) {
         debug() << "Failed getaddrinfo with error: " << gai_strerror(errorCode) << std::endl;
@@ -88,7 +110,7 @@ void blocking_client(const bool &stopped, uint64_t &bytesRead) {
     freeaddrinfo(result);
 
     if (result == nullptr) {
-        info() << "Could not connect to: " << host << ":" << port << std::endl;
+        info() << "Could not connect to: " << SERVER_HOST << ":" << SERVER_PORT << std::endl;
     }
 
     std::vector<char> buf(FOUR_KiB);
@@ -100,11 +122,11 @@ void blocking_client(const bool &stopped, uint64_t &bytesRead) {
             numReceivedBytes += bufferSizeReceived;
             if (bufferSizeReceived == -1) {
                 /// if read method returned -1 an error occurred during read.
-                info() << "An error occurred while reading from socket. Error: " << strerror(errno);
+                // info() << "An error occurred while reading from socket. Error: " << strerror(errno);
                 break;
             }
             if (bufferSizeReceived == 0) {
-                info() << "No data received from " << host << ":" << port;
+                info() << "No data received from " << SERVER_HOST << ":" << SERVER_PORT;
             }
         }
         bytesRead += numReceivedBytes;
@@ -118,9 +140,10 @@ void blocking_client(const bool &stopped, uint64_t &bytesRead) {
 
 asio::awaitable<void> listener(const int num_connections) try {
     const auto executor = co_await asio::this_coro::executor;
-    tcp::acceptor acceptor{executor, {{}, port}};
+    tcp::acceptor acceptor{executor, {asio::ip::address_v4::from_string("127.0.0.1"), SERVER_PORT}};
     for (int i = 0; i < num_connections; ++i) {
-        co_spawn(executor, send(co_await acceptor.async_accept()), asio::detached);
+        auto socket = co_await acceptor.async_accept();
+        co_spawn(executor, send(std::move(socket)), asio::detached);
     }
 } catch (asio::system_error const &se) {
     debug() << "listener: " << se.code().message() << std::endl;
@@ -128,25 +151,39 @@ asio::awaitable<void> listener(const int num_connections) try {
 
 int main(const int argc, char **argv) {
     asio::thread_pool ioc;
-    const int numConnections = std::stoi(argv[2]);
-    std::vector<uint64_t> bytesRead(numConnections, 0);
+    const int num_connections = std::stoi(argv[2]);
+    std::vector<uint64_t> bytesRead(num_connections, 0);
 
     if (const std::string executable = argv[1]; executable == "s") {
-        co_spawn(ioc, listener(numConnections), asio::detached);
+        co_spawn(ioc, listener(num_connections), asio::detached);
     } else {
-        const int duration = std::stoi(argv[3]);
         bool stopped = false;
+        const int duration = std::stoi(argv[3]);
 
         if (executable == "ac") {
-            for (int i = 0; i < numConnections; ++i) {
-                co_spawn(ioc, async_client(stopped, bytesRead.at(i)), asio::detached);
+            int ip_suffix = 2;
+            for (int i = 0; i < num_connections; ++i) {
+                tcp::endpoint client_endpoint{
+                    asio::ip::address_v4::from_string(std::format("127.0.0.{}", ip_suffix)),
+                    static_cast<uint16_t>(i + 10000 % UINT16_MAX)
+                };
+                co_spawn(ioc, async_client(std::move(client_endpoint), num_connections, bytesRead.at(i)),
+                         asio::detached);
+                if (i % UINT16_MAX == 0) {
+                    ip_suffix++;
+                }
             }
+
+            while (num_connected < num_connections) {
+                std::this_thread::sleep_for(10ms);
+            }
+            info() << "All clients connected, waiting for " << duration << "s..." << std::endl;
             std::this_thread::sleep_for(std::chrono::seconds(duration));
-            stopped = true;
+            shutdown_signal.store(true);
         } else if (executable == "bc") {
             std::vector<std::jthread> threads;
-            threads.reserve(numConnections);
-            for (int i = 0; i < numConnections; ++i) {
+            threads.reserve(num_connections);
+            for (int i = 0; i < num_connections; ++i) {
                 threads.emplace_back(blocking_client, std::ref(stopped), std::ref(bytesRead.at(i)));
             }
 
